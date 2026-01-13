da2473578a41aeb2a4a43ae677c9bb8fe4c2c492
AdeleM-prog <12043@holbertonstudents.com>
2026-01-13 08:49:04 +0000

    crea dir + factorial.py

---
+++ debugging/factorial.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+    if n < 0:
+        raise ValueError("factorial() not defined for negative values")
+
+    result = 1
+    while n > 1:
+        result *= n
+        n -= 1
+    return result
+
+if __name__ == "__main__":
+    f = factorial(int(sys.argv[1]))
+    print(f)
